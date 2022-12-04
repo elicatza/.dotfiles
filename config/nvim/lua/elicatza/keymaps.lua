ae20aafc980eff9e4d2d83ae1eb0dc5cf2f3ebc8
Eliza <eliza.cl@protonmail.com>
2022-12-04 08:58:57 +0100

    feat: updated <C-d> and <C-u> to center screen

--- config/nvim/lua/elicatza/keymaps.lua
+++ config/nvim/lua/elicatza/keymaps.lua
@@ -8,6 +8,8 @@ vim.api.nvim_set_keymap("i", "æ", "<Esc>", { noremap = true })
 vim.api.nvim_set_keymap("i", "æ", "<Esc>", { noremap = true })
 vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv=gv", { noremap = true})
 vim.api.nvim_set_keymap("v", "K", ":m '>-2<CR>gv=gv", { noremap = true})
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
 
 vim.api.nvim_set_keymap("n", "<leader>vrc", ":e " .. CONF_DIR .. "/nvim/init.lua<CR>", { noremap = true })
 
